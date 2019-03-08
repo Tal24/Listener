b0464d11320be398002db6bd01ef71a2d7b137a8
Tal24 <talshoef@mail.tau.ac.il>
2019-03-08 17:29:33 +0200

    Docker integration

--- src/main/java/com/tsts/listener/couchbase/CouchbaseConfiguration.java
+++ src/main/java/com/tsts/listener/couchbase/CouchbaseConfiguration.java
@@ -4,6 +4,7 @@ package com.tsts.listener.couchbase;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.couchbase.CouchbaseAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 
 import java.util.Collections;
@@ -11,11 +12,18 @@ import java.util.List;
 
 @ConditionalOnProperty(value = "db", havingValue = "couchbase")
 @ImportAutoConfiguration(CouchbaseAutoConfiguration.class)
+@ConfigurationProperties(prefix = "spring.data.couchbase")
 public class CouchbaseConfiguration extends AbstractCouchbaseConfiguration {
+
+    private String host;
+
+    public void setHost (String host) {
+        this.host = host;
+    }
 
     @Override
     protected List<String> getBootstrapHosts () {
-        return Collections.singletonList("localhost");
+        return Collections.singletonList(host);
     }
 
     @Override
@@ -25,6 +33,6 @@ public class CouchbaseConfiguration extends AbstractCouchbaseConfiguration {
 
     @Override
     protected String getBucketPassword () {
-        return "Administrator";
+        return "listener";
     }
 }
