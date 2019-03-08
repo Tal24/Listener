38ae0592d933e030c84848aefa12ba692768273e
Tal24 <talshoef@mail.tau.ac.il>
2019-03-08 11:48:26 +0200

    Database setup

--- src/main/java/com/tsts/listener/ListenerApplication.java
+++ src/main/java/com/tsts/listener/ListenerApplication.java
@@ -8,11 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, CouchbaseAutoConfiguration.class})
+@EnableAutoConfiguration
 @SpringBootConfiguration
 @ImportAutoConfiguration(classes = {MongoConfiguration.class, CouchbaseConfiguration.class})
 public class ListenerApplication {
