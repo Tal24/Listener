5643fc390bf04c14c47b871a4c25dd2ba48f25b8
Tal24 <talshoef@mail.tau.ac.il>
2019-03-08 12:00:10 +0200

    Database setup

--- src/main/java/com/tsts/listener/couchbase/CouchbaseConfiguration.java
+++ src/main/java/com/tsts/listener/couchbase/CouchbaseConfiguration.java
@@ -1,16 +1,13 @@
 package com.tsts.listener.couchbase;
 
 
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseAutoConfiguration;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 
 import java.util.Collections;
 import java.util.List;
 
 @ConditionalOnProperty(value = "db", havingValue = "couchbase")
-@ImportAutoConfiguration(classes = {CouchbaseAutoConfiguration.class})
 public class CouchbaseConfiguration extends AbstractCouchbaseConfiguration {
 
     @Override
